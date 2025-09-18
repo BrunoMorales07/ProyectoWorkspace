3257fed4810cf70873dc684e3611569ffe60d029
BrunoMorales07 <bruno.morales.ia2016@gmail.com>
2025-09-18 09:15:47 -0300

    ACtu

--- js/products.js
+++ js/products.js
@@ -1,23 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
+    //Agregamos el catID del localStorage
+    const categoriaID =localStorage.getItem('catID');
+    //Almacenamos la URL
+    const PRODUCTS_URL =`https://japceibal.github.io/emercado-api/cats_products/${categoriaID}.json`;
+
     const contenedor = document.getElementById('contenedor');
     contenedor.className = 'row';
-    
-    fetch('https://japceibal.github.io/emercado-api/cats_products/101.json')
-        .then(response => {
-            console.log("GET exitoso:", response.status);
-            return response.json();
-        })
-        .then(data => {
+    //Agregamos las funciones del spinner
+    let showSpinner = function(){
+  document.getElementById("spinner-wrapper").style.display = "block";
+}
 
-    data.products.forEach(product => {
+let hideSpinner = function(){
+  document.getElementById("spinner-wrapper").style.display = "none";
+}
+
+//Agregamos el fetch de init
+let getJSONData = function(url){
+    let result = {};
+    showSpinner();
+    return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw Error(response.statusText);
+      }
+    })
+    .then(function(response) {
+          result.status = 'ok';
+          result.data = response;
+    
+            contenedor.innerHTML ='';
+    const products =response.products || response;
+
+    products.forEach(product => {
+    localStorage.setItem("ID", product.id);
     const elementoProducto  = document.createElement('div');
-    elementoProducto.className = 'col-lg-6 col-md-12  mb-4'; 
+    elementoProducto.className = 'col-lg-4 col-md-6 col-sm-12  mb-4'; 
     
     elementoProducto.innerHTML = `
-        <div class="card h-100" style=" border: 1px solid black;">
-            <div class="card-header bg-danger text-white text-center">
-                <h5 class="card-title mb-0 text-white fw-bold  py-2 rounded">${product.name}</h5>
+        <div class="card h-100 rounded-4 card-efect " style=" border: 1px solid black;">
+            <div class="card-header text-white text-center" style="background: var(--red-gradient);">
+                <h5 class="card-title mb-0 text-white fw-bold  py-2 rounded fs-4">${product.name}</h5>
             </div>
             <img src="${product.image}" 
                  class="card-img-top" 
@@ -25,26 +50,45 @@ document.addEventListener('DOMContentLoaded', function() {
                  style="height: 200px; object-fit: cover; border-top: 2px solid red; border-bottom: 2px solid red;">
             <div class="card-body bg-light">
                 <p class="card-text text-dark fw-bold">${product.description}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <span class="badge bg-danger fs-6 text-dark" style="border: 1px solid black;">
-                        Precio: ${product.cost} ${product.currency}
+                <div class="d-flex flex-column flex-sm-row justify-content-between align-items-center">
+                    <span class="badge fs-6 text-white" style=" background:var(--red-gradient); border: 1px solid black;">
+                      Precio: ${product.cost} ${product.currency}
                     </span>
                     <small class="text-dark fw-bold">Vendidos: ${product.soldCount}</small>
                 </div>
             </div>
-            <div class="card-footer bg-danger text-center" style="border-top: 2px solid black;">
-                <button class="btn btn-outline-dark fw-bold" style="background-color: white;">
-                    comprar
+            <div class="card-footer text-center" style="background:transparent;">
+                <button class="fw-bold btnProducts show-btn"  id="${product.id}">
+                    Ver Mas
                 </button>
             </div>
         </div>
     `;
     
     contenedor.appendChild(elementoProducto);
-}); 
 });
-        })
-        .catch(error => {
-            console.error("Hubo un problema con la petición fetch:", error);
-            contenedor.innerHTML = "<p>No se pudo cargar la lista de productos.</p>";
-        });
+
+ hideSpinner();
+          return result;
+    })
+
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;
+        hideSpinner();
+        return result;
+    });
+};
+getJSONData(PRODUCTS_URL).then(function() {
+
+  let arrayButtons = document.getElementsByClassName('btnProducts');
+  for (let index = 0; index < arrayButtons.length; index++) {
+    const button = arrayButtons[index];
+    button.addEventListener('click', function(e) {
+      localStorage.setItem("ID", button.id);
+      location.href = "product-info.html";
+    
+    })
+  }
+})
+});
